fe252394c3269a2f587845268df654331a9ef11b
respat <respergerpatrik@gmail.com>
2023-11-30 15:50:46 +0100

    Megjegyzések hozzáadva

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -1,3 +1,13 @@
+/*
+ * File: Controller.java
+ * Author: Resperger Patrik
+ * Copyright: 2023, Resperger Patrik
+ * Group: Szoft II/1/E
+ * Date: 2023-11-30
+ * Github: https://github.com/respat/
+ * Licenc: GNU GPL
+ */
+
 package org.example;
 
 public class Controller implements Controllable {
